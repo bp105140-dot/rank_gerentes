fd0405ae95c1de86a996d26b908e949c928a2cf7
bp105140-dot <bp105140@gmail.com>
2026-07-10 20:40:58 -0300

    Comment out middleware authentication logic

--- middleware.js
+++ middleware.js
@@ -1,28 +1,28 @@
-export const config = {
-  // O asterisco protege todas as rotas e arquivos do site
-  matcher: '/:path*',
-};
+// export const config = {
+//   // O asterisco protege todas as rotas e arquivos do site
+//   matcher: '/:path*',
+// };
 
-export default function middleware(request) {
-  const authorizationHeader = request.headers.get('authorization');
+// export default function middleware(request) {
+//   const authorizationHeader = request.headers.get('authorization');
 
-  if (authorizationHeader) {
-    const basicAuth = authorizationHeader.split(' ')[1];
-    const [user, password] = atob(basicAuth).split(':');
+//   if (authorizationHeader) {
+//     const basicAuth = authorizationHeader.split(' ')[1];
+//     const [user, password] = atob(basicAuth).split(':');
 
-    // Troque 'admin' e 'senha123' pelo usuário e senha que você quiser
-    if (user === 'admin' && password === 'Levy084611#') {
-      return new Response(null, {
-        headers: { 'x-middleware-next': '1' } // Permite o acesso
-      });
-    }
-  }
+//     // Troque 'admin' e 'senha123' pelo usuário e senha que você quiser
+//     if (user === 'admin' && password === 'Levy084611#') {
+//       return new Response(null, {
+//         headers: { 'x-middleware-next': '1' } // Permite o acesso
+//       });
+//     }
+//   }
 
-  // Se a senha estiver errada ou não for inserida, barra o usuário
-  return new Response('Acesso Negado. Credenciais inválidas.', {
-    status: 401,
-    headers: {
-      'WWW-Authenticate': 'Basic realm="Área Restrita do Projeto"',
-    },
-  });
-}
+//   // Se a senha estiver errada ou não for inserida, barra o usuário
+//   return new Response('Acesso Negado. Credenciais inválidas.', {
+//     status: 401,
+//     headers: {
+//       'WWW-Authenticate': 'Basic realm="Área Restrita do Projeto"',
+//     },
+//   });
+// }
